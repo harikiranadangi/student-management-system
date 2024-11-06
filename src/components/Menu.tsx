7a4689cba2f3dd80797c431e04b1e61d9ad1e73b
Hari Kiran Adangi <harikiran.adangi@gmail.com>
2024-11-06 19:28:09 +0530

    Create data.ts & Update Menu.tsx

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,34 +1,145 @@
+import { role } from "@/lib/data";
 import Image from "next/image";
 import Link from "next/link";
 
 const menuItems = [
-  { icon: "/home.png", label: "Home", href: "/" },
-  { icon: "/teacher.png", label: "Teachers", href: "/teachers" },
-  { icon: "/student.png", label: "Students", href: "/students" },
-  { icon: "/parent.png", label: "Parents", href: "/parents" },
-  { icon: "/subject.png", label: "Subjects", href: "/subjects" },
-  { icon: "/class.png", label: "Classes", href: "/classes" },
-  { icon: "/lesson.png", label: "Lessons", href: "/lessons" },
-  { icon: "/exam.png", label: "Exams", href: "/exams" },
-  { icon: "/assignment.png", label: "Assignments", href: "/assignments" },
-  { icon: "/result.png", label: "Results", href: "/results" },
-  { icon: "/attendance.png", label: "Attendance", href: "/attendance" },
-  { icon: "/calendar.png", label: "Events", href: "/events" },
-  { icon: "/message.png", label: "Messages", href: "/messages" },
-  { icon: "/announcement.png", label: "Announcements", href: "/announcements" },
-  { icon: "/profile.png", label: "Profile", href: "/profile" },
-  { icon: "/setting.png", label: "Settings", href: "/settings" },
-  { icon: "/logout.png", label: "Logout", href: "/logout" },
+  {
+    title: "MENU",
+    items: [
+      {
+        icon: "/home.png",
+        label: "Home",
+        href: "/",
+        visible: ["admin", "teacher", "student", "parent"],
+      },
+      {
+        icon: "/teacher.png",
+        label: "Teachers",
+        href: "/list/teachers",
+        visible: ["admin", "teacher"],
+      },
+      {
+        icon: "/student.png",
+        label: "Students",
+        href: "/list/students",
+        visible: ["admin", "teacher"],
+      },
+      {
+        icon: "/parent.png",
+        label: "Parents",
+        href: "/list/parents",
+        visible: ["admin", "teacher"],
+      },
+      {
+        icon: "/subject.png",
+        label: "Subjects",
+        href: "/list/subjects",
+        visible: ["admin"],
+      },
+      {
+        icon: "/class.png",
+        label: "Classes",
+        href: "/list/classes",
+        visible: ["admin", "teacher"],
+      },
+      {
+        icon: "/lesson.png",
+        label: "Lessons",
+        href: "/list/lessons",
+        visible: ["admin", "teacher"],
+      },
+      {
+        icon: "/exam.png",
+        label: "Exams",
+        href: "/list/exams",
+        visible: ["admin", "teacher", "student", "parent"],
+      },
+      {
+        icon: "/assignment.png",
+        label: "Assignments",
+        href: "/list/assignments",
+        visible: ["admin", "teacher", "student", "parent"],
+      },
+      {
+        icon: "/result.png",
+        label: "Results",
+        href: "/list/results",
+        visible: ["admin", "teacher", "student", "parent"],
+      },
+      {
+        icon: "/attendance.png",
+        label: "Attendance",
+        href: "/list/attendance",
+        visible: ["admin", "teacher", "student", "parent"],
+      },
+      {
+        icon: "/calendar.png",
+        label: "Events",
+        href: "/list/events",
+        visible: ["admin", "teacher", "student", "parent"],
+      },
+      {
+        icon: "/message.png",
+        label: "Messages",
+        href: "/list/messages",
+        visible: ["admin", "teacher", "student", "parent"],
+      },
+      {
+        icon: "/announcement.png",
+        label: "Announcements",
+        href: "/list/announcements",
+        visible: ["admin", "teacher", "student", "parent"],
+      },
+    ],
+  },
+  {
+    title: "OTHER",
+    items: [
+      {
+        icon: "/profile.png",
+        label: "Profile",
+        href: "/profile",
+        visible: ["admin", "teacher", "student", "parent"],
+      },
+      {
+        icon: "/setting.png",
+        label: "Settings",
+        href: "/settings",
+        visible: ["admin", "teacher", "student", "parent"],
+      },
+      {
+        icon: "/logout.png",
+        label: "Logout",
+        href: "/logout",
+        visible: ["admin", "teacher", "student", "parent"],
+      },
+    ],
+  },
 ];
 
 const Menu = () => {
   return (
     <div className="mt-4 text-sm">
-      {menuItems.map((item) => (
-        <Link href={item.href} key={item.label} className="flex items-center justify-center gap-2 p-2 rounded lg:justify-start hover:bg-gray-100">
-          <Image src={item.icon} alt={item.label} width={20} height={20} />
-          <span className="hidden lg:block">{item.label}</span>
-        </Link>
+      {menuItems.map((i) => (
+        <div className="flex flex-col gap-2" key={i.title}>
+          <span className="hidden my-4 font-light text-gray-400 lg:block">
+            {i.title}
+          </span>
+          {i.items.map((item) => {
+            if (item.visible.includes(role)) {
+              return (
+                <Link 
+                  href={item.href}
+                  key={item.label}
+                  className="flex items-center justify-center gap-4 py-2 text-gray-500 rounded-md lg:justify-start md:px-2 hover:bg-lamaSkyLight"
+                >
+                  <Image src={item.icon} alt="" width={20} height={20} />
+                  <span className="hidden lg:block">{item.label}</span>
+                </Link>
+              );
+            }
+          })}
+        </div>
       ))}
     </div>
   );
---
+++ src/lib/data.ts
@@ -0,0 +1,53 @@
+// TEMPORARY DATA
+
+export let role = "admin";
+
+export const teachersData = [
+
+];
+
+export const studentsData = [
+  
+];
+
+export const parentsData = [
+  
+];
+
+export const subjectsData = [
+  
+];
+
+export const classesData = [
+  
+];
+
+export const lessonsData = [
+
+];
+
+export const examsData = [
+  
+];
+
+export const assignmentsData = [
+  
+];
+
+export const resultsData = [
+  
+];
+
+export const eventsData = [
+  
+];
+
+export const announcementsData = [
+
+];
+
+
+// YOU SHOULD CHANGE THE DATES OF THE EVENTS TO THE CURRENT DATE TO SEE THE EVENTS ON THE CALENDAR
+export const calendarEvents = [
+  
+];
