353da7f85ecd41e5cdbd47ca500d0bdb1e84bfb0
Hari Kiran Adangi <harikiran.adangi@gmail.com>
2024-11-07 19:04:33 +0530

    Update BigCalendar.tsx

--- src/components/BigCalendar.tsx
+++ src/components/BigCalendar.tsx
@@ -23,7 +23,8 @@ const BigCalendar = () => {
                 endAccessor="end"
                 views={{ work_week: true, day: true }}
                 view={view}
-                style={{height: "98%"}}
+                style={{
+                    height: "98%"}}
                 onView={handleOnChangeView}
                 min={new Date(2025, 1, 0, 8, 50, 0)}
                 max={new Date(2025, 1, 0, 16, 15, 0)}
