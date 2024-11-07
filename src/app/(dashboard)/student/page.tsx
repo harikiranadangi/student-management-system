8705881c6e5d875c5d7ae77b3e040526dd1dd7d9
Hari Kiran Adangi <harikiran.adangi@gmail.com>
2024-11-07 06:47:49 +0530

    Create BigCalendar.tsx

--- src/app/(dashboard)/student/page.tsx
+++ src/app/(dashboard)/student/page.tsx
@@ -1,6 +1,23 @@
+import Announcements from "@/components/Announcements";
+import BigCalendar from "@/components/BigCalendar";
+import EventCalendar from "@/components/EventCalendar";
+
 const StudentPage = () => {
   return (
-    <div className="container">StudentPage</div>
+    <div className="flex flex-col gap-4 p-4 xl:flex-row">
+      {/* LEFT: Description */}
+      <div className='w-full xl:w-2/3'>
+        <div className='h-full p-4 bg-white rounded-md'>
+          <h1 className="text-xl font-semibold">Schedule (4A)</h1>
+          <BigCalendar/>
+        </div>
+      </div>
+      {/* RIGHT: Description */}
+        <div className='flex flex-col w-full gap-8 xl:w-1/3'>
+        <EventCalendar />
+        <Announcements />
+        </div>
+    </div>
   );
 };
 
--- src/lib/data.ts
+++ src/lib/data.ts
@@ -48,6 +48,132 @@ export const announcementsData = [
 
 
 // YOU SHOULD CHANGE THE DATES OF THE EVENTS TO THE CURRENT DATE TO SEE THE EVENTS ON THE CALENDAR
+// Define events for November 2024
 export const calendarEvents = [
-  
+    {
+        id: 1,
+        title: "Math Class",
+        class: "5B",
+        start: new Date("2024-11-01T09:00:00"),
+        end: new Date("2024-11-01T10:00:00"),
+    },
+    {
+        id: 2,
+        title: "Science Class",
+        class: "5B",
+        start: new Date("2024-11-01T10:00:00"),
+        end: new Date("2024-11-01T11:00:00"),
+    },
+    {
+        id: 3,
+        title: "English Class",
+        class: "5B",
+        start: new Date("2024-11-01T11:00:00"),
+        end: new Date("2024-11-01T12:00:00"),
+    },
+    {
+        id: 4,
+        title: "History Class",
+        class: "5B",
+        start: new Date("2024-11-01T12:00:00"),
+        end: new Date("2024-11-01T13:00:00"),
+    },
+    {
+        id: 5,
+        title: "Physical Education",
+        class: "5B",
+        start: new Date("2024-11-02T09:00:00"),
+        end: new Date("2024-11-02T10:00:00"),
+    },
+    {
+        id: 6,
+        title: "Art Class",
+        class: "5B",
+        start: new Date("2024-11-02T10:00:00"),
+        end: new Date("2024-11-02T11:00:00"),
+    },
+    {
+        id: 7,
+        title: "Sports Tournament",
+        class: "5B",
+        start: new Date("2024-11-02T11:00:00"),
+        end: new Date("2024-11-02T12:00:00"),
+    },
+    {
+        id: 8,
+        title: "Math Class",
+        class: "5B",
+        start: new Date("2024-11-03T09:00:00"),
+        end: new Date("2024-11-03T10:00:00"),
+    },
+    {
+        id: 9,
+        title: "Science Class",
+        class: "5B",
+        start: new Date("2024-11-03T10:00:00"),
+        end: new Date("2024-11-03T11:00:00"),
+    },
+    {
+        id: 10,
+        title: "English Class",
+        class: "5B",
+        start: new Date("2024-11-03T11:00:00"),
+        end: new Date("2024-11-03T12:00:00"),
+    },
+    {
+        id: 11,
+        title: "History Class",
+        class: "5B",
+        start: new Date("2024-11-03T12:00:00"),
+        end: new Date("2024-11-03T13:00:00"),
+    },
+    {
+        id: 12,
+        title: "Sports Tournament",
+        class: "5B",
+        start: new Date("2024-11-04T09:00:00"),
+        end: new Date("2024-11-04T10:00:00"),
+    },
+    {
+        id: 13,
+        title: "Physical Education",
+        class: "5B",
+        start: new Date("2024-11-04T10:00:00"),
+        end: new Date("2024-11-04T11:00:00"),
+    },
+    {
+        id: 14,
+        title: "Art Class",
+        class: "5B",
+        start: new Date("2024-11-04T11:00:00"),
+        end: new Date("2024-11-04T12:00:00"),
+    },
+    {
+        id: 15,
+        title: "Math Class",
+        class: "5B",
+        start: new Date("2024-11-05T09:00:00"),
+        end: new Date("2024-11-05T10:00:00"),
+    },
+    {
+        id: 16,
+        title: "Science Class",
+        class: "5B",
+        start: new Date("2024-11-05T10:00:00"),
+        end: new Date("2024-11-05T11:00:00"),
+    },
+    {
+        id: 17,
+        title: "Sports Tournament",
+        class: "5B",
+        start: new Date("2024-11-06T09:00:00"),
+        end: new Date("2024-11-06T10:00:00"),
+    },
+    {
+        id: 18,
+        title: "Physical Education",
+        class: "5B",
+        start: new Date("2024-11-06T10:00:00"),
+        end: new Date("2024-11-06T11:00:00"),
+    },
 ];
