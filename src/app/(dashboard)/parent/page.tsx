494d04485b3cac70cf20df8368d45e2c339118ba
Hari Kiran Adangi <harikiran.adangi@gmail.com>
2024-11-08 05:48:46 +0530

    Create Parent Page

--- src/app/(dashboard)/parent/page.tsx
+++ src/app/(dashboard)/parent/page.tsx
@@ -1,13 +1,13 @@
 import Announcements from "@/components/Announcements";
 import BigCalendar from "@/components/BigCalendar";
 
-const TeacherPage = () => {
+const ParentPage = () => {
   return (
     <div className="flex flex-col flex-1 gap-4 p-4 xl:flex-row">
       {/* LEFT: Description */}
       <div className='w-full xl:w-2/3'>
         <div className='h-full p-4 bg-white rounded-md'>
-          <h1 className="text-xl font-semibold">Schedule</h1>
+          <h1 className="text-xl font-semibold">Schedule (Aswini)</h1>
           <BigCalendar/>
         </div>
       </div>
@@ -19,4 +19,4 @@ const TeacherPage = () => {
   );
 };
 
-export default TeacherPage;
+export default ParentPage;
