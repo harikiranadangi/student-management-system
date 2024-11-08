f8364b7669dd34da22e331120a6c55efed142378
Hari Kiran Adangi <harikiran.adangi@gmail.com>
2024-11-08 20:06:28 +0530

    Update page.tsx

--- src/app/(dashboard)/list/teachers/page.tsx
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -1,16 +1,104 @@
 import Pagination from "@/components/Pagination";
+import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
+import { role, teachersData } from "@/lib/data";
 import Image from "next/image";
+import Link from "next/link";
+
+type Teacher = {
+  id: number;
+  teacherId: string;
+  name: string;
+  email?: string;
+  photo: string;
+  phone: string;
+  subjects: string[];
+  classes: string[];
+  address: string;
+};
+
+const columns = [
+  {
+    header: "Info",
+    accessor: "info",
+  },
+  {
+    header: "Teacher ID",
+    accessor: "teacherId",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "Subjects",
+    accessor: "subjects",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "Classes",
+    accessor: "classes",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "Phone",
+    accessor: "phone",
+    className: "hidden lg:table-cell",
+  },
+  {
+    header: "Address",
+    accessor: "address",
+    className: "hidden lg:table-cell",
+  },
+  {
+    header: "Actions",
+    accessor: "action",
+  },
+];
 
 const TeacherList = () => {
+  const renderRow = (item: Teacher) => (
+    <tr key={item.id}>
+      <td>
+        <Image
+          src={item.photo}
+          alt=""
+          width={40}
+          height={40}
+          className="object-cover w-10 h-10 rounded-full md:hidden xl:block"
+        />
+        <div className="flex flex-col">
+          <h3 className="font-semibold">{item.name}</h3>
+          <p className="text-xs text-gray-500">{item?.email}</p>
+        </div>
+      </td>
+      <td className="hidden md:table-cell">{item.teacherId}</td>
+      <td className="hidden md:table-cell">{item.subjects.join(", ")}</td>
+      <td className="hidden md:table-cell">{item.classes.join(", ")}</td>
+      <td className="hidden md:table-cell">{item.phone}</td>
+      <td className="hidden md:table-cell">{item.address}</td>
+      <td>
+        <div className="flex items-center gap-2">
+          <Link href={`/list/teachers/${item.id}`}>
+            <button className="flex items-center justify-center rounded-full w-7 h-7 bg-LamaSky">
+              <Image src="/view.png" alt="" width={16} height={16} />
+            </button>
+          </Link>
+          {role === "admin" && (
+            <button className="flex items-center justify-center rounded-full w-7 h-7 bg-LamaPurple">
+              <Image src="/delete.png" alt="" width={16} height={16} />
+            </button>
+          )}
+        </div>
+      </td>
+    </tr>
+  );
+
   return (
     <div className="flex-1 p-4 m-4 mt-0 bg-white rounded-md">
       {/* TOP: Description */}
-      <div className='flex items-center justify-between'>
+      <div className="flex items-center justify-between">
         <h1 className="hidden text-lg font-semibold md:block">All Teachers</h1>
-        <div className='flex flex-col items-center w-full gap-4 md:flex-row md:w-auto'>
-          <TableSearch  />
-          <div className='flex items-center self-end gap-4'>
+        <div className="flex flex-col items-center w-full gap-4 md:flex-row md:w-auto">
+          <TableSearch />
+          <div className="flex items-center self-end gap-4">
             <button className="flex items-center justify-center w-8 h-8 rounded-full bg-LamaYellow">
               <Image src="/filter.png" alt="" width={14} height={14} />
             </button>
@@ -20,14 +108,13 @@ const TeacherList = () => {
             <button className="flex items-center justify-center w-8 h-8 rounded-full bg-LamaYellow">
               <Image src="/create.png" alt="" width={14} height={14} />
             </button>
-          
           </div>
         </div>
       </div>
       {/* LIST: Description */}
-      {/* PAGINATIOM: Description */}
-        <Pagination/>
-
+      <Table columns={columns} renderRow={renderRow} data={teachersData} />
+      {/* PAGINATION: Description */}
+      <Pagination />
     </div>
   );
 };
