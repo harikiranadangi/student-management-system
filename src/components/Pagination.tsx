ea872a8753201894da2642d23174d7136c1a57ed
Hari Kiran Adangi <harikiran.adangi@gmail.com>
2024-11-08 19:26:26 +0530

    Update Pagination.tsx

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -10,7 +10,7 @@ const Pagination = () => {
             <button className="px-2 rounded-sm">10</button>
 
         </div>
-        <button className="px-4 py-2 text-xs font-semibold rounded-md bg-slate-200 disabled:opacity-50 disabled:cursor-not-allowed">Prev</button>
+        <button className="px-4 py-2 text-xs font-semibold rounded-md bg-slate-200 disabled:opacity-50 disabled:cursor-not-allowed">Next</button>
     </div> 
   );
 };
