56aaab3884e8636d054a1667d26bba8507f3ff5c
Hari Kiran Adangi <harikiran.adangi@gmail.com>
2024-11-09 22:38:12 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images:{
-    remotePatterns:[{hostname:"images.pexels.com"}],
+  images: {
+    remotePatterns: [
+      { hostname: "images.pexels.com" },
+      
+    ],
   },
 };
 
