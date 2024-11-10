b464c7cc2348699fa8613e534e90a46caead7473
Hari Kiran Adangi <harikiran.adangi@gmail.com>
2024-11-10 10:15:57 +0530

    Update page.tsx

--- src/app/(dashboard)/list/announcements/page.tsx
+++ src/app/(dashboard)/list/announcements/page.tsx
@@ -1,3 +1,4 @@
+import FormModal from "@/components/FormModal";
 import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
@@ -38,19 +39,15 @@ const AnnouncementsList = () => {
   const renderRow = (item: Announcements) => (
     <tr key={item.id} className="text-sm border-b border-gray-200 even:bg-slate-50 hover:bg-LamaPurpleLight" >
       <td className="flex items-center gap-4 p-4">{item.title }</td>
-      <td>{item.class}</td>
+      <td>  {item.class}</td>
       <td className="hidden md:table-cell">{item.date }</td>
       <td>
         <div className="flex items-center gap-2">
-          <Link href={`/list/teachers/${item.id}`}>
-            <button className="flex items-center justify-center rounded-full w-7 h-7 bg-LamaSky">
-              <Image src="/update.png" alt="Edit" width={16} height={16} />
-            </button>
-          </Link>
-          {role === "admin" && (
-            <button className="flex items-center justify-center rounded-full w-7 h-7 bg-LamaPurple">
-              <Image src="/delete.png" alt="" width={16} height={16} />
-            </button>
+        {role === "admin" && (
+              <>
+              <FormModal table="announcement" type="update" data={item} className="p-2 text-white rounded-full bg-LamaSky" /> 
+              <FormModal table="announcement" type="delete" className="p-2 text-white rounded-full bg-LamaSky" /> 
+             </>
           )}
         </div>
       </td>
@@ -72,9 +69,7 @@ const AnnouncementsList = () => {
               <Image src="/sort.png" alt="" width={14} height={14} />
             </button>
             {role === "admin" && (
-                <button className="flex items-center justify-center w-8 h-8 rounded-full bg-LamaYellow">
-                    <Image src="/create.png" alt="" width={14} height={14} />
-                </button>
+                <FormModal table="announcement" type="create" className="p-2 text-white rounded-full bg-LamaYellow" /> 
             )}
           </div>
         </div>
