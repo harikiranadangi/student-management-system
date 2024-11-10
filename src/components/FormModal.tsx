7098469c69e56543bf384a89409b09ee31aab200
Hari Kiran Adangi <harikiran.adangi@gmail.com>
2024-11-10 10:27:52 +0530

    Create FormModal.tsx

---
+++ src/components/FormModal.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import dynamic from "next/dynamic";
+import Image from "next/image";
+import { useState } from "react";
+
+const FormModal = ({
+  table,
+  type,
+  data,
+  id,
+  className = "",
+}: {
+  table:
+    | "teacher"
+    | "student"
+    | "parent"
+    | "subject"
+    | "class"
+    | "lesson"
+    | "exam"
+    | "assignment"
+    | "result"
+    | "attendance"
+    | "event"
+    | "announcement";
+  type: "create" | "update" | "delete";
+  data?: any;
+  id?: number;
+  className?: string; // Add className as an optional prop
+}) => {
+  const size = type === "create" ? "w-8 h-8" : "w-7 h-7";
+  const bgColor = type === "create" ? "bg-lamaYellow" : type === "update" ? "bg-lamaSky" : "bg-lamaPurple";
+
+  const [open, setOpen] = useState(false);
+
+  const Form = () => {
+    return type === "delete" && id ? (
+      <form action="" className="flex flex-col gap-4 p-4">
+        <span className="font-medium text-center">
+          All data will be lost. Are you sure you want to delete this {table}?
+        </span>
+        <button className="self-center px-4 py-2 text-white bg-red-700 border-none rounded-md w-max">
+          Delete
+        </button>
+      </form>
+    ) : type === "create" || type === "update" ? (
+      forms[table](type, data)
+    ) : (
+      "Form not found!"
+    );
+  };
+
+  return (
+    <>
+      <button
+        className={`${size} flex items-center justify-center rounded-full ${bgColor} ${className}`}
+        onClick={() => setOpen(true)}
+      >
+        <Image src={`/${type}.png`} alt="" width={16} height={16} />
+      </button>
+      {open && (
+        <div className="absolute top-0 left-0 z-50 flex items-center justify-center w-screen h-screen bg-black bg-opacity-60">
+          <div className="bg-white p-4 rounded-md relative w-[90%] md:w-[70%] lg:w-[60%] xl:w-[50%] 2xl:w-[40%]">
+            <Form />
+            <div
+              className="absolute cursor-pointer top-4 right-4"
+              onClick={() => setOpen(false)}
+            >
+              <Image src="/close.png" alt="" width={14} height={14} />
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default FormModal;
