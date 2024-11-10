6218af73936f615ee76393d96a45e86d92f4c43d
Hari Kiran Adangi <harikiran.adangi@gmail.com>
2024-11-10 16:49:34 +0530

    Update InputField.tsx

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -19,7 +19,13 @@ const InputField = ({
     error,
     inputProps
 }: InputFieldProps) => {
-  return <div className="container">Content</div>
+  return (
+    <div className='flex flex-col gap-2 md:w-1/4'>
+        <label className="text-xs text-gray-500">Username</label>
+        <input type="text" {...register("username")} className="ring-[1.5px] ring-gray-300 p-2 rounded-md text-sm w-full" />
+        {errors.username?.message && <p className="text-xs text-red-400">{errors.name?.message?.toString()}</p>}
+        </div>
+  );
   
 };
 
