26cac773bb1219c2a00c2b912f6f10615176600f
Hari Kiran Adangi <harikiran.adangi@gmail.com>
2024-11-10 10:17:06 +0530

    Update page.tsx

--- src/app/(dashboard)/list/lessons/page.tsx
+++ src/app/(dashboard)/list/lessons/page.tsx
@@ -1,3 +1,4 @@
+import FormModal from "@/components/FormModal";
 import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
@@ -41,15 +42,11 @@ const LessonsList = () => {
       <td className="hidden md:table-cell">{item.teacher }</td>
       <td>
         <div className="flex items-center gap-2">
-          <Link href={`/list/teachers/${item.id}`}>
-            <button className="flex items-center justify-center rounded-full w-7 h-7 bg-LamaSky">
-              <Image src="/update.png" alt="Edit" width={16} height={16} />
-            </button>
-          </Link>
           {role === "admin" && (
-            <button className="flex items-center justify-center rounded-full w-7 h-7 bg-LamaPurple">
-              <Image src="/delete.png" alt="" width={16} height={16} />
-            </button>
+            <>
+              <FormModal table="lesson" type="update" data={item} className="p-2 text-white rounded-full bg-LamaSky" /> 
+              <FormModal table="lesson" type="delete" className="p-2 text-white rounded-full bg-LamaSky" /> 
+          </>
           )}
         </div>
       </td>
@@ -71,9 +68,7 @@ const LessonsList = () => {
               <Image src="/sort.png" alt="" width={14} height={14} />
             </button>
             {role === "admin" && (
-                <button className="flex items-center justify-center w-8 h-8 rounded-full bg-LamaYellow">
-                    <Image src="/create.png" alt="" width={14} height={14} />
-                </button>
+                <FormModal table="lesson" type="create" className="p-2 text-white rounded-full bg-LamaYellow" /> 
             )}
           </div>
         </div>
