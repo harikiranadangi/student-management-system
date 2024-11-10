52a909ffd7ed80c0a94060d99ba16b96e29307c1
Hari Kiran Adangi <harikiran.adangi@gmail.com>
2024-11-10 16:47:13 +0530

    Update InputField.tsx

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,7 +1,26 @@
-const InputField = () => {
-  return (
-    <div className="container">Content</div>
-  );
+import { FieldError } from "react-hook-form";
+
+type InputFieldProps ={
+    label:string;
+    type?:any;
+    register:string;
+    name:string;
+    defaultValue:string;
+    error:FieldError;
+    inputProps:React.InputHTMLAttributes<HTMLInputElement>;
+}
+
+const InputField = ({
+    label,
+    type = 'text',
+    register,
+    name,
+    defaultValue,
+    error,
+    inputProps
+}: InputFieldProps) => {
+  return <div className="container">Content</div>
+  
 };
 
 export default InputField;
