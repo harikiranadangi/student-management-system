0d4056637705213bf07405868d5a719c8b9b66e9
Hari Kiran Adangi <harikiran.adangi@gmail.com>
2024-11-11 18:56:35 +0530

    Update page.tsx

--- src/app/(dashboard)/list/results/page.tsx
+++ src/app/(dashboard)/list/results/page.tsx
@@ -4,7 +4,6 @@ import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
 import { resultsData,  role} from "@/lib/data";
 import Image from "next/image";
-import Link from "next/link";
 
 type Results = {
   id: number;
