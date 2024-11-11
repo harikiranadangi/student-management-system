b59ae0e90c3b06338bbae46ad01bde95293041c3
Hari Kiran Adangi <harikiran.adangi@gmail.com>
2024-11-11 06:14:08 +0530

    Update page.tsx

--- src/app/(dashboard)/list/Exams/page.tsx
+++ src/app/(dashboard)/list/Exams/page.tsx
@@ -51,8 +51,8 @@ const ExamsList = () => {
         <div className="flex items-center gap-2">
           {role === "admin" && (
               <>
-              <FormModal table="event" type="update" data={item} className="p-2 text-white rounded-full bg-LamaSky" /> 
-              <FormModal table="event" type="delete" className="p-2 text-white rounded-full bg-LamaSky" /> 
+              <FormModal table="exam" type="update" data={item}/> 
+              <FormModal table="exam" type="delete"  id={item.id} /> 
              </>
           )}
         </div>
@@ -75,7 +75,7 @@ const ExamsList = () => {
               <Image src="/sort.png" alt="" width={14} height={14} />
             </button>
             {role === "admin" && (
-                <FormModal table="event" type="create" className="p-2 text-white rounded-full bg-LamaYellow" /> 
+                <FormModal table="exam" type="create"/> 
             )}
           </div>
         </div>
