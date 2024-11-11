8ade26d6c5b87aab87bcd3052d54faa78ae252db
Hari Kiran Adangi <harikiran.adangi@gmail.com>
2024-11-11 18:56:02 +0530

    Update page.tsx

--- src/app/(dashboard)/list/results/page.tsx
+++ src/app/(dashboard)/list/results/page.tsx
@@ -64,10 +64,11 @@ const ResultsList = () => {
     <tr key={item.id} className="text-sm border-b border-gray-200 even:bg-slate-50 hover:bg-LamaPurpleLight" >
       <td className="flex items-center gap-4 p-4">{item.subject}</td>
       <td >{item.student }</td>
-      <td className="hidden md:table-cell">{item.score }</td>
+      <td className="hidden md:table-cell">{item.score}</td>
       <td className="hidden md:table-cell">{item.teacher }</td>
       <td className="hidden md:table-cell">{item.class}</td>
-      <td className="hidden md:table-cell">{item.date }</td>
+      <td className="hidden md:table-cell">{item.date}</td>
+      <td className="hidden md:table-cell">{item.type}</td>
       <td>
         <div className="flex items-center gap-2">
         {role === "admin" && (
