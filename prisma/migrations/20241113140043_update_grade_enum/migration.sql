84aed152f9e4cc8e2bf1c9e541ef84a988825749
Hari Kiran Adangi <harikiran.adangi@gmail.com>
2024-11-13 22:32:32 +0530

    delete migration

--- prisma/migrations/20241113140043_update_grade_enum/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - The values [FIRST,SECOND,THIRD,FOURTH,FIFTH,SIXTH,SEVENTH,EIGHTH,NINTH,TENTH,ELEVENTH,TWELFTH] on the enum `Students_grade` will be removed. If these variants are still used in the database, this will fail.
-
-*/
--- AlterTable
-ALTER TABLE `students` MODIFY `grade` ENUM('PRE_KG', 'LKG', 'UKG', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X') NOT NULL;
