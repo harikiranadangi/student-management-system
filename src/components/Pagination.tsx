4030060bc83cb2af84b50fe141288e3ed4acdf59
Hari Kiran Adangi <harikiran.adangi@gmail.com>
2024-11-14 20:29:31 +0530

    Update Pagination.tsx

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,6 +1,18 @@
 // \src\components\Pagination.tsx
+"use client"
+import { ITEM_PER_PAGE } from "@/lib/settings";
+import { useRouter } from "next/router";
 
-const Pagination = () => {
+const Pagination = ({page, count}:{page: number; count:number}) => {
+
+  const router = useRouter()
+
+  const changePage = (newPage:number) =>{
+    const params = new URLSearchParams(window.location.search)
+    params.set("page", newPage.toString());
+    router.push(`${window.location.pathname}?${params}`);
+  }
+
   return (
     <div className="flex items-center justify-between p-4 text-gray-500">
         <button 
@@ -11,11 +23,20 @@ const Pagination = () => {
         </button>
         
         <div className='flex items-center gap-2 text-sm'>
-            <button className="px-2 rounded-sm bg-LamaSky">1</button>
-            <button className="px-2 rounded-sm">2</button>
-            <button className="px-2 rounded-sm">3</button>
-            ...
-            <button className="px-2 rounded-sm">10</button>
+            {Array.from
+              ({length:Math.ceil(count/ITEM_PER_PAGE)}, 
+              (_, index)=>{
+                const pageIndex = index + 1;
+                return (
+                  <button key={pageIndex} className={`px-2 rounded-sm ${page === pageIndex ? "bg-LamaSky" : ""
+                    }`} 
+                    onClick={()=>{changePage(pageIndex)}}
+                  >
+                    {pageIndex}
+                  </button>
+                );
+              }
+            )}
         </div>
         <button className="px-4 py-2 text-xs font-semibold rounded-md bg-slate-200 disabled:opacity-50 disabled:cursor-not-allowed">
           Next
