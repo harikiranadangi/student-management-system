9df81a8f669c23c387284f9c34e935c19a64a211
Hari Kiran Adangi <harikiran.adangi@gmail.com>
2024-11-15 19:56:11 +0530

    Create page.tsx

---
+++ src/app/(dashboard)/list/students/[id]/page.tsx
@@ -0,0 +1,142 @@
+import Announcements from "@/components/Announcements";
+import BigCalendar from "@/components/BigCalendar";
+import Performance from "@/components/Performance";
+import Image from "next/image";
+import Link from "next/link";
+
+const SingleStudentPage = () => {
+  return (
+    <div className="flex flex-col flex-1 gap-4 p-4 xl:flex-row">
+      {/* LEFT */}
+      <div className="w-full xl:w-2/3">
+        {/* TOP */}
+        <div className="flex flex-col gap-4 lg:flex-row">
+          {/* USER INFO CARD */}
+          <div className="flex flex-1 gap-4 px-4 py-6 rounded-md bg-lamaSky">
+            <div className="w-1/3">
+              <Image
+                src="https://images.pexels.com/photos/5414817/pexels-photo-5414817.jpeg?auto=compress&cs=tinysrgb&w=1200"
+                alt=""
+                width={144}
+                height={144}
+                className="object-cover rounded-full w-36 h-36"
+              />
+            </div>
+            <div className="flex flex-col justify-between w-2/3 gap-4">
+              <h1 className="text-xl font-semibold">Cameron Moran</h1>
+              <p className="text-sm text-gray-500">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              </p>
+              <div className="flex flex-wrap items-center justify-between gap-2 text-xs font-medium">
+                <div className="flex items-center w-full gap-2 md:w-1/3 lg:w-full 2xl:w-1/3">
+                  <Image src="/blood.png" alt="" width={14} height={14} />
+                  <span>A+</span>
+                </div>
+                <div className="flex items-center w-full gap-2 md:w-1/3 lg:w-full 2xl:w-1/3">
+                  <Image src="/date.png" alt="" width={14} height={14} />
+                  <span>January 2025</span>
+                </div>
+                <div className="flex items-center w-full gap-2 md:w-1/3 lg:w-full 2xl:w-1/3">
+                  <Image src="/mail.png" alt="" width={14} height={14} />
+                  <span>user@gmail.com</span>
+                </div>
+                <div className="flex items-center w-full gap-2 md:w-1/3 lg:w-full 2xl:w-1/3">
+                  <Image src="/phone.png" alt="" width={14} height={14} />
+                  <span>+1 234 567</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* SMALL CARDS */}
+          <div className="flex flex-wrap justify-between flex-1 gap-4">
+            {/* CARD */}
+            <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
+              <Image
+                src="/singleAttendance.png"
+                alt=""
+                width={24}
+                height={24}
+                className="w-6 h-6"
+              />
+              <div className="">
+                <h1 className="text-xl font-semibold">90%</h1>
+                <span className="text-sm text-gray-400">Attendance</span>
+              </div>
+            </div>
+            {/* CARD */}
+            <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
+              <Image
+                src="/singleBranch.png"
+                alt=""
+                width={24}
+                height={24}
+                className="w-6 h-6"
+              />
+              <div className="">
+                <h1 className="text-xl font-semibold">6th</h1>
+                <span className="text-sm text-gray-400">Grade</span>
+              </div>
+            </div>
+            {/* CARD */}
+            <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
+              <Image
+                src="/singleLesson.png"
+                alt=""
+                width={24}
+                height={24}
+                className="w-6 h-6"
+              />
+              <div className="">
+                <h1 className="text-xl font-semibold">18</h1>
+                <span className="text-sm text-gray-400">Lessons</span>
+              </div>
+            </div>
+            {/* CARD */}
+            <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
+              <Image
+                src="/singleClass.png"
+                alt=""
+                width={24}
+                height={24}
+                className="w-6 h-6"
+              />
+              <div className="">
+                <h1 className="text-xl font-semibold">6A</h1>
+                <span className="text-sm text-gray-400">Class</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* BOTTOM */}
+        <div className="mt-4 bg-white rounded-md p-4 h-[800px]">
+          <h1>Student&apos;s Schedule</h1>
+          <BigCalendar />
+        </div>
+      </div>
+      {/* RIGHT */}
+      <div className="flex flex-col w-full gap-4 xl:w-1/3">
+        <div className="p-4 bg-white rounded-md">
+          <h1 className="text-xl font-semibold">Shortcuts</h1>
+          <div className="flex flex-wrap gap-4 mt-4 text-xs text-gray-500">
+            <Link className="p-3 rounded-md bg-lamaPurpleLight" href={'/list/teachers?classId=${2}'}>
+              Student&apos;s Teachers
+            </Link>
+            <Link className="p-3 rounded-md bg-pink-50" href="/">
+              Student&apos;s Exams
+            </Link>
+            <Link className="p-3 rounded-md bg-lamaSkyLight" href="/">
+              Student&apos;s Assignments
+            </Link>
+            <Link className="p-3 rounded-md bg-lamaYellowLight" href="/">
+              Student&apos;s Results
+            </Link>
+          </div>
+        </div>
+        <Performance />
+        <Announcements />
+      </div>
+    </div>
+  );
+};
+
+export default SingleStudentPage;
