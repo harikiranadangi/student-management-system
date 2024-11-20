fd22082822784e5e1e8ff842f2ce4751178704b1
Hari Kiran Adangi <harikiran.adangi@gmail.com>
2024-11-20 20:25:19 +0530

    Update page.tsx

--- src/app/[[...sign-in]]/page.tsx
+++ src/app/[[...sign-in]]/page.tsx
@@ -2,36 +2,45 @@
 
 import * as Clerk from '@clerk/elements/common'
 import * as SignIn from '@clerk/elements/sign-in'
+import { useUser } from '@clerk/nextjs'
+import { useRouter } from 'next/navigation'
+import { useEffect } from 'react'
 
-export default function SignInPage() {
+export default function LoginPage() {
+
+  const { isLoaded, isSignedIn, user } = useUser()
+
+  const router = useRouter()
+
+  useEffect(()=>{
+    const role = user?.publicMetadata.role;
+
+    if(role){
+      router.push(`/${role}`)
+    }
+  }, [user, router]);
+
   return (
     <div className="flex items-center justify-center w-full min-h-screen px-4 bg-gray-100">
       <SignIn.Root>
         <SignIn.Step
           name="start"
-          className="flex flex-col gap-4 p-12 bg-white rounded-md shadow-2xl"
+          className="flex flex-col gap-2 p-12 bg-white rounded-md shadow-2xl"
         >
           <header className="text-center">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 40 40"
-              className="mx-auto size-10 text-zinc-950"
-              aria-hidden
-            >
-              <mask id="a" width="40" height="40" x="0" y="0" maskUnits="userSpaceOnUse">
-                <circle cx="20" cy="20" r="20" fill="#D9D9D9" />
-              </mask>
-              <g fill="currentColor" mask="url(#a)">
-                <path d="M43.5 3a.5.5 0 0 0 0-1v1Zm0-1h-46v1h46V2ZM43.5 8a.5.5 0 0 0 0-1v1Zm0-1h-46v1h46V7ZM43.5 13a.5.5 0 0 0 0-1v1Zm0-1h-46v1h46v-1ZM43.5 18a.5.5 0 0 0 0-1v1Zm0-1h-46v1h46v-1ZM43.5 23a.5.5 0 0 0 0-1v1Zm0-1h-46v1h46v-1ZM43.5 28a.5.5 0 0 0 0-1v1Zm0-1h-46v1h46v-1ZM43.5 33a.5.5 0 0 0 0-1v1Zm0-1h-46v1h46v-1ZM43.5 38a.5.5 0 0 0 0-1v1Zm0-1h-46v1h46v-1Z" />
-                <path d="M27 3.5a1 1 0 1 0 0-2v2Zm0-2h-46v2h46v-2ZM25 8.5a1 1 0 1 0 0-2v2Zm0-2h-46v2h46v-2ZM23 13.5a1 1 0 1 0 0-2v2Zm0-2h-46v2h46v-2ZM21.5 18.5a1 1 0 1 0 0-2v2Zm0-2h-46v2h46v-2ZM20.5 23.5a1 1 0 1 0 0-2v2Zm0-2h-46v2h46v-2ZM22.5 28.5a1 1 0 1 0 0-2v2Zm0-2h-46v2h46v-2ZM25 33.5a1 1 0 1 0 0-2v2Zm0-2h-46v2h46v-2ZM27 38.5a1 1 0 1 0 0-2v2Zm0-2h-46v2h46v-2Z" />
-              </g>
-            </svg>
-            <h1 className="mt-4 text-xl font-medium tracking-tight text-zinc-950">
-              Sign in to Kotak Salesian School
+            <img
+              src="/logo.png"
+              alt="Kotak Salesian School Logo"
+              className="w-20 h-20 mx-auto"
+            />
+            <h1 className="items-center gap-2 text-xl text-center font-mediumbold ">
+              Kotak Salesian School
             </h1>
+            <h2 className='text-gray-500'>
+              Sign in to your account
+            </h2>
           </header>
-          <Clerk.GlobalError className="block text-sm text-red-400" />
+          <Clerk.GlobalError className="text-sm text-red-400" />
           <div className="space-y-4">
             <Clerk.Field name="identifier" className="space-y-2">
               <Clerk.Label className="text-sm font-medium text-zinc-950">Username</Clerk.Label>
@@ -40,7 +49,7 @@ export default function SignInPage() {
                 required
                 className="w-full rounded-md bg-white px-3.5 py-2 text-sm outline-none ring-1 ring-inset ring-zinc-300 hover:ring-zinc-400 focus:ring-[1.5px] focus:ring-zinc-950 data-[invalid]:ring-red-400"
               />
-              <Clerk.FieldError className="block text-sm text-red-400" />
+              <Clerk.FieldError className="text-sm text-red-400" />
             </Clerk.Field>
             <Clerk.Field name="password" className="space-y-2">
               <Clerk.Label className="text-sm font-medium text-zinc-950">Password</Clerk.Label>
