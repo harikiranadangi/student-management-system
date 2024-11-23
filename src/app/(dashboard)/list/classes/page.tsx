f9d8774f5726137ed904ba90f1da3f999b5e9308
Hari Kiran Adangi <harikiran.adangi@gmail.com>
2024-11-24 00:28:08 +0530

    Update page.tsx

--- src/app/(dashboard)/list/classes/page.tsx
+++ src/app/(dashboard)/list/classes/page.tsx
@@ -2,65 +2,104 @@ import FormModal from "@/components/FormModal";
 import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
-import { classesData, role} from "@/lib/data";
+import { classesData, role } from "@/lib/data";
+import prisma from "@/lib/prisma";
+import { ITEM_PER_PAGE } from "@/lib/settings";
+import { Class, Prisma, Teacher } from "@prisma/client";
 import Image from "next/image";
 import Link from "next/link";
 
-type Class = {
-  id: number;
-  name: string;
-  capacity: number;
-  grade: number;
-  supervisor: string;
-};
+type ClassList = Class & {supervisor:Teacher}
 
 const columns = [
-    {
-      header: "Class Name",
-      accessor: "name",
-    },
-    {
-      header: "Capacity",
-      accessor: "capacity",
-      className: "hidden md:table-cell",
-    },
-    {
-      header: "Grade",
-      accessor: "grade",
-      className: "hidden md:table-cell",
-    },
-    {
-      header: "Supervisor",
-      accessor: "supervisor",
-      className: "hidden md:table-cell",
-    },
-    {
-      header: "Actions",
-      accessor: "action",
-    },
-  ];
-  
+  {
+    header: "Class Name",
+    accessor: "name",
+  },
+  // {
+  //   header: "Capacity",
+  //   accessor: "capacity",
+  //   className: "hidden md:table-cell",
+  // },
+  // {
+  //   header: "Grade",
+  //   accessor: "grade",
+  //   className: "hidden md:table-cell",
+  // },
+  {
+    header: "Supervisor",
+    accessor: "supervisor",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "Actions",
+    accessor: "action",
+  },
+];
 
-const ClassesList = () => {
-  const renderRow = (item: Class) => (
-    <tr key={item.id} className="text-sm border-b border-gray-200 even:bg-slate-50 hover:bg-LamaPurpleLight" >
-      <td className="flex items-center gap-4 p-4">{item.name}</td>
-      <td className="hidden md:table-cell">{item.capacity}</td>
-      <td className="hidden md:table-cell">{item.grade }</td>
-      <td className="hidden md:table-cell">{item.supervisor}</td>
-      <td>
-        <div className="flex items-center gap-2">
-          {role === "admin" && (
-            <>
-              <FormModal table="class" type="update" data={item}  /> 
-              <FormModal table="class" type="delete" id={item.id} /> 
-            </>
-            
-          )}
-        </div>
-      </td>
-    </tr>
-  );
+
+const renderRow = (item: ClassList) => (
+  <tr key={item.id} className="text-sm border-b border-gray-200 even:bg-slate-50 hover:bg-LamaPurpleLight" >
+    <td className="flex items-center gap-4 p-4">{item.name}</td>
+    {/* <td className="hidden md:table-cell">{item.capacity}</td>
+    <td className="hidden md:table-cell">{item.name[0]}</td> */}
+    <td className="hidden md:table-cell">{item.supervisor.name + " " + item.supervisor.surname}</td>
+    <td>
+      <div className="flex items-center gap-2">
+        {role === "admin" && (
+          <>
+            <FormModal table="class" type="update" data={item} />
+            <FormModal table="class" type="delete" id={item.id} />
+          </>
+
+        )}
+      </div>
+    </td>
+  </tr>
+);
+
+
+const ClassesList = async ({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) => {
+  const { page, ...queryParams } = searchParams;
+  const p = page ? parseInt(page) : 1;
+
+  // Initialize Prisma query object
+  const query: Prisma.ClassWhereInput = {};
+
+  // Dynamically add filters based on query parameters
+  if (queryParams) {
+    for (const [key, value] of Object.entries(queryParams)) {
+      if (value !== undefined) {
+        switch (key) {
+          case "supervisorId":
+            query.supervisorId = parseInt(value);
+            break;
+          case "search":
+            query.name = { contains: value };
+            break;
+          default:
+            break;
+        }
+      }
+    }
+  }
+
+  // Fetch teachers and include related fields (subjects, classes)
+  const [data, count] = await prisma.$transaction([
+    prisma.class.findMany({
+      where: query,
+      include: {
+        supervisor: true,
+      },
+      take: ITEM_PER_PAGE,
+      skip: ITEM_PER_PAGE * (p - 1),
+    }),
+    prisma.class.count({ where: query }),
+  ]);
 
   return (
     <div className="flex-1 p-4 m-4 mt-0 bg-white rounded-md">
@@ -77,15 +116,15 @@ const ClassesList = () => {
               <Image src="/sort.png" alt="" width={14} height={14} />
             </button>
             {role === "admin" && (
-                <FormModal table="class" type="create"  /> 
+              <FormModal table="class" type="create" />
             )}
           </div>
         </div>
       </div>
       {/* LIST: Description */}
-      <Table columns={columns} renderRow={renderRow} data={classesData} />
+      <Table columns={columns} renderRow={renderRow} data={data} />
       {/* PAGINATION: Description */}
-      <Pagination />
+      <Pagination page={p} count={count} />
     </div>
   );
 };
