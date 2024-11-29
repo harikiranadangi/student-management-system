4f98ca2a0c2d1b1990f8d5441589377fb6fe6057
Hari Kiran Adangi <harikiran.adangi@gmail.com>
2024-11-30 00:39:15 +0530

    Update UserCard.tsx

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,6 +1,19 @@
+import prisma from "@/lib/prisma";
 import Image from "next/image";
 
-const UserCard = ({ type }: { type: string }) => {
+const UserCard = async ({ type }: { type: "admin" | "teacher" | "student" }) => {
+
+  const modelMap : Record<typeof type, any> = {
+    admin: prisma.admin,
+    teacher: prisma.teacher,
+    student: prisma.student,
+  };
+
+  const data = await modelMap[type].count();
+
+  console.log(data)
+
+
   return (
     <div className="rounded-2xl odd:bg-LamaPurple even:bg-LamaYellow p-4 flex-1 min-w-[130px]">
       <div className="flex items-center justify-between">
@@ -9,7 +22,7 @@ const UserCard = ({ type }: { type: string }) => {
         </span>
         <Image src="/more.png" alt="More" width={20} height={20} />
       </div>
-      <h1 className="my-4 text-2xl font-semibold">1,234</h1>
+      <h1 className="my-4 text-2xl font-semibold">{data}</h1>
       <h2 className="text-sm font-medium text-gray-500 capitalize">{type}s</h2>
     </div>
   );
