3231d1e317ac77291eb921566efeb8f5ee26f5e1
Hari Kiran Adangi <harikiran.adangi@gmail.com>
2024-11-30 07:32:13 +0530

    Update AttendanceChart.tsx

--- src/components/AttendanceChart.tsx
+++ src/components/AttendanceChart.tsx
@@ -1,24 +1,13 @@
 "use client";
 
-import Image from 'next/image';
 import { BarChart, Bar, Rectangle, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
-const data = [
-  { name: 'Mon', present: 1000, absent: 735 },
-  { name: 'Tue', present: 1200, absent: 535 },
-  { name: 'Wed', present: 1300, absent: 435 },
-  { name: 'Thur', present: 1500, absent: 235 },
-  { name: 'Fri', present: 1400, absent: 335 },
-  { name: 'Sat', present: 1100, absent: 635 },
-];
+
  
-const AttendanceChart = () => {
+const AttendanceChart = ({data}: {data: {name:string, present:number, absent:number}[]
+}) => {
   return (
-    <div className='h-full p-4 bg-white rounded-lg'>
-      <div className="flex items-center justify-between">
-        <h1 className='text-lg font-semibold'> Attendance</h1>
-        <Image src="/moreDark.png" alt="" width={20} height={20} />
-      </div>
+    
       <ResponsiveContainer width="100%" height="90%">
         <BarChart width={500} height={300} data={data} barSize={20}>
           <CartesianGrid strokeDasharray="3 3" vertical={false} stroke='#ddd'/>
@@ -30,7 +19,7 @@ const AttendanceChart = () => {
           <Bar dataKey="absent" fill="#C3EBFA" legendType="circle" radius={[10,10,0,0]}/>
         </BarChart>
       </ResponsiveContainer>
-    </div>
+    
   );
 };
 
