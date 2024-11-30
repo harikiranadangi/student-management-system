d14856ba53496127b35e8fb59a860cb258a161ad
Hari Kiran Adangi <harikiran.adangi@gmail.com>
2024-11-30 11:00:11 +0530

    Update page.tsx

--- src/app/(dashboard)/teacher/page.tsx
+++ src/app/(dashboard)/teacher/page.tsx
@@ -1,14 +1,17 @@
 import Announcements from "@/components/Announcements";
-import BigCalendar from "@/components/BigCalendar";
+import BigCalendarContainer from "@/components/BigCalendarContainer";
+import { auth } from "@clerk/nextjs/server";
 
-const TeacherPage = () => {
+const TeacherPage = async () => {
+
+  const { userId } = await auth();
   return (
     <div className="flex flex-col flex-1 gap-4 p-4 xl:flex-row">
       {/* LEFT: Description */}
       <div className='w-full xl:w-2/3'>
         <div className='h-full p-4 bg-white rounded-md'>
           <h1 className="text-xl font-semibold">Schedule</h1>
-          <BigCalendar/>
+          <BigCalendarContainer type="teacherId" id={userId!}/>
         </div>
       </div>
       {/* RIGHT: Description */}
