acbd1a112161026da8072099a8709fecfd98fb48
Hari Kiran Adangi <harikiran.adangi@gmail.com>
2024-11-30 11:00:16 +0530

    Create BigCalendarContainer.tsx

---
+++ src/components/BigCalendarContainer.tsx
@@ -0,0 +1,31 @@
+import prisma from "@/lib/prisma";
+import BigCalendar from "./BigCalendar";
+
+const BigCalendarContainer = async ({
+    type,
+    id,
+}: {
+    type: "teacherId" | "class";
+    id: string | number;
+}) => {
+
+    const dataRes = await prisma.lesson.findMany({
+        where: {
+            ...(type === "teacherId" 
+                ? {teacherId :id as string}
+            : { classId: id as number }),
+        },
+    });
+
+    const data = dataRes.map(lesson=> ({
+        title: lesson.name,
+        start: lesson.startTime,
+        end: lesson.endTime,
+    }))
+    
+  return (
+    <div className="container"><BigCalendar data={data}/></div>
+  );
+};
+
+export default BigCalendarContainer;
