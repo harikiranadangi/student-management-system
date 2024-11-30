725ab535cc5a7ecaed16b05506f2ae95f3dd2ce8
Hari Kiran Adangi <harikiran.adangi@gmail.com>
2024-11-30 19:50:00 +0530

    Create SubjectForm.tsx

---
+++ src/components/forms/SubjectForm.tsx
@@ -0,0 +1,13 @@
+const SubjectForm = ({ 
+    type, 
+    data, 
+}: { 
+    type: "create" | "update"; 
+    data?: any;
+ }) => {
+    return (
+        <div className="container">Content</div>
+    );
+};
+
+export default SubjectForm;
