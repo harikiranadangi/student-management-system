79145b3d987c034334b3901a1b18900dbeb30088
Hari Kiran Adangi <harikiran.adangi@gmail.com>
2024-11-30 19:49:46 +0530

    Update page.tsx

--- src/app/(dashboard)/list/subjects/page.tsx
+++ src/app/(dashboard)/list/subjects/page.tsx
@@ -2,9 +2,9 @@ import FormModal from "@/components/FormModal";
 import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
-import {  role, subjectsData } from "@/lib/data";
 import prisma from "@/lib/prisma";
 import { ITEM_PER_PAGE } from "@/lib/settings";
+import { getRole } from "@/lib/utils";
 import { Prisma, Subject, Teacher } from "@prisma/client";
 import Image from "next/image";
 
@@ -36,6 +36,8 @@ type SubjectList = Subject & {teachers:Teacher[]}
   }: {
     searchParams: { [key: string]: string | undefined };
   }) => {
+
+    const role = await getRole();
 
     const columns = [
       {
