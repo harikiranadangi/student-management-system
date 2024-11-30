c5fcd04fb849c6ae1039b8a0e500d3fe8c1736de
Hari Kiran Adangi <harikiran.adangi@gmail.com>
2024-11-30 10:16:15 +0530

    Update Announcements.tsx

--- src/components/Announcements.tsx
+++ src/components/Announcements.tsx
@@ -1,46 +1,76 @@
-const Announcements = () => {
-  return (
-    <div className="p-4 bg-white rounded-md">
-        <div className='flex items-center justify-between'>
-            <h1 className="text-xl font-semibold">Announcements</h1>
-            <span className="text-xs text-gray-400">View All</span>
+import prisma from "@/lib/prisma";
+import { getRole } from "@/lib/utils";
+import { auth } from "@clerk/nextjs/server";
+
+const Announcements = async () => {
+
+    const { userId } = await auth();
+
+    const role = await getRole()
+
+    const roleConditions = {
+        admin: {},
+        teacher: { lessons: { some: { teacherId: userId! } } },
+        student: { lessons: { some: { id: userId! } } },
+    }
+
+
+    const data = await prisma.announcement.findMany({
+        take: 3,
+        orderBy: { date: "desc" },
+        where: {
+            ...(role !== "admin" && {
+                OR: [{ classId: null },
+                { class: roleConditions[role as keyof typeof roleConditions] || {} },
+                ],
+            }),
+        }
+    });
+
+    return (
+        <div className="p-4 bg-white rounded-md">
+            <div className='flex items-center justify-between'>
+                <h1 className="text-xl font-semibold">Announcements</h1>
+                <span className="text-xs text-gray-400">View All</span>
+            </div>
+            <div className='flex flex-col gap-4 mt-4'>
+                {data[0] && <div className='p-4 rounded-md bg-LamaSkyLight'>
+                    <div className='flex items-center justify-between'>
+                        <h2 className="font-medium">{data[0].title}</h2>
+                        <span className="px-1 py-1 text-xs text-gray-400 bg-white rounded-md">
+                            {new Intl.DateTimeFormat("en-GB").format(data[0].date)}
+                        </span>
+                    </div>
+                    <p className="mt-1 text-sm text-gray-400">
+                        {data[0].description}
+                    </p>
+                </div>}
+
+                {data[1] && <div className='p-4 rounded-md bg-LamaPurple'>
+                    <div className='flex items-center justify-between'>
+                        <h2 className="font-medium">{data[1].title}</h2>
+                        <span className="px-1 py-1 text-xs text-gray-500 bg-white rounded-md">
+                            {new Intl.DateTimeFormat("en-GB").format(data[1].date)}
+                        </span>
+                    </div>
+                    <p className="mt-1 text-sm text-gray-400">
+                        {data[1].description}
+                    </p>
+                </div>}
+
+                {data[2] && <div className='p-4 rounded-md bg-LamaYellow'>
+                    <div className='flex items-center justify-between'>
+                        <h2 className="font-medium">{data[2].title}</h2>
+                        <span className="px-1 py-1 text-xs text-gray-500 bg-white rounded-md">{new Intl.DateTimeFormat("en-GB").format(data[2].date)}</span>
+                    </div>
+                    <p className="mt-1 text-sm text-gray-500">
+                        {data[2].title}
+                    </p>
+                </div>}
+
+            </div>
         </div>
-        <div className='flex flex-col gap-4 mt-4'>
-            <div className='p-4 rounded-md bg-LamaSkyLight'>
-                <div className='flex items-center justify-between'>
-                    <h2 className="font-medium">🎓 Upcoming Parent-Teacher Conferences</h2>
-                    <span className="px-1 py-1 text-xs text-gray-400 bg-white rounded-md">
-                    2025-01-01
-                    </span>
-                </div>
-                <p className="mt-1 text-sm text-gray-400">
-                    Parent-Teacher Conferences are scheduled for November 15-17. We encourage all parents to attend and discuss their child’s progress with the teachers. Please book your slot through the school portal.
-                </p>
-            </div>
-            <div className='p-4 rounded-md bg-LamaPurple'>
-                <div className='flex items-center justify-between'>
-                    <h2 className="font-medium">📚 Library Book Fair</h2>
-                    <span className="px-1 py-1 text-xs text-gray-500 bg-white rounded-md">
-                    2025-01-01
-                    </span>
-                </div>
-                <p className="mt-1 text-sm text-gray-400">
-                Our annual Library Book Fair is back! Students and parents are invited to explore a variety of books and resources. Special discounts will be available. Don’t miss out!  
-                </p>
-                
-            </div>
-            <div className='p-4 rounded-md bg-LamaYellow'>
-                <div className='flex items-center justify-between'>
-                    <h2 className="font-medium">🏆 Sports Day Celebration</h2>
-                    <span className="px-1 py-1 text-xs text-gray-500 bg-white rounded-md">2025-01-01</span>
-                </div>
-                <p className="mt-1 text-sm text-gray-500">
-                Get ready for our annual Sports Day on December 5! Students will compete in various sports and games. Families are invited to join and cheer for their kids. Let’s make it a memorable event!    
-                </p>
-            </div>
-        </div>
-    </div>
-  );
+    );
 };
 
 export default Announcements;
