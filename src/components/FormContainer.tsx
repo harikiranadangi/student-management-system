ab5bba4be3eb0a236a97ba00ec4b19ea7d55f52a
Hari Kiran Adangi <harikiran.adangi@gmail.com>
2024-12-01 18:12:45 +0530

    Create FormContainer.tsx

---
+++ src/components/FormContainer.tsx
@@ -0,0 +1,35 @@
+import FormModal from "./FormModal";
+
+export type FormContainerProps = {
+    table:
+    | "teacher"
+    | "student"
+    | "parent"
+    | "subject"
+    | "class"
+    | "lesson"
+    | "exam"
+    | "assignment"
+    | "result"
+    | "attendance"
+    | "event"
+    | "announcement";
+    type: "create" | "update" | "delete";
+    data?: any;
+    id?: number | string;
+  }
+
+const FormContainer = ({
+    table,
+    type,
+    data,
+    id,
+  }: FormContainerProps ) => {
+  return (
+    <div className="container"><FormModal table={table} type={type} data={data} id={id}/>
+    
+    </div>
+  );
+};
+
+export default FormContainer;
