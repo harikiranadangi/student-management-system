6a336d0ebef4ce8a2282104e145b8987d3e3b35d
Hari Kiran Adangi <harikiran.adangi@gmail.com>
2024-12-03 20:55:14 +0530

    Update actions.ts

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,74 +1,209 @@
 "use server"
 
 import { revalidatePath } from "next/cache";
-import { SubjectSchema } from "./formValidationSchemas"
+import { ClassSchema, SubjectSchema, Teacherschema } from "./formValidationSchemas"
 import prisma from "./prisma"
 
-type CurrentState = {success:boolean;error:boolean}
+type CurrentState = { success: boolean; error: boolean }
+
+
+// * ---------------------------------------------- SUBJECT SCHEMA --------------------------------------------------------
 
 export const createSubject = async (
-    currentState: CurrentState, 
-    data:SubjectSchema
+    currentState: CurrentState,
+    data: SubjectSchema
 ) => {
-    try{
+    try {
         await prisma.subject.create({
-            data: { 
+            data: {
                 name: data.name,
-                teachers:{
+                teachers: {
                     connect: data.teachers.map((teacherId) => ({ id: teacherId })),
                 }
             },
         });
-     
+
         // revalidatePath("/list/subjects")
-        return { success:true, error: false};
-    }   catch (err) {
+        return { success: true, error: false };
+    } catch (err) {
         console.log(err)
-        return { success:false, error: true};
+        return { success: false, error: true };
     }
 };
 
 export const updateSubject = async (
-    currentState: CurrentState, 
-    data:SubjectSchema
+    currentState: CurrentState,
+    data: SubjectSchema
 ) => {
-    try{
+    try {
         await prisma.subject.update({
             where: {
                 id: data.id,
             },
-            data: { 
+            data: {
                 name: data.name,
                 teachers: {
                     set: data.teachers.map((teacherId) => ({ id: teacherId })),
                 }
             },
         });
-     
+
         // revalidatePath("/list/subjects")
-        return { success:true, error: false};
-    }   catch (err) {
+        return { success: true, error: false };
+    } catch (err) {
         console.log(err)
-        return { success:false, error: true};
+        return { success: false, error: true };
     }
 };
 
 export const deleteSubject = async (
-    currentState: CurrentState, 
-    data:FormData
+    currentState: CurrentState,
+    data: FormData
 ) => {
     const id = data.get("id") as string;
-    try{
+    try {
         await prisma.subject.delete({
             where: {
                 id: parseInt(id)
             },
         });
-     
+
         // revalidatePath("/list/subjects")
-        return { success:true, error: false};
-    }   catch (err) {
+        return { success: true, error: false };
+    } catch (err) {
         console.log(err)
-        return { success:false, error: true};
+        return { success: false, error: true };
     }
 };
+
+// * ---------------------------------------------- CLASS SCHEMA --------------------------------------------------------
+
+export const createClass = async (
+    currentState: CurrentState,
+    data: ClassSchema
+) => {
+    try {
+        await prisma.class.create({
+            data: {
+                name: data.name,
+                supervisorId: data.supervisorId,
+                gradeId: data.gradeId,
+            },
+        });
+
+        // revalidatePath("/list/class")
+        return { success: true, error: false };
+    } catch (err) {
+        console.log(err)
+        return { success: false, error: true };
+    }
+};
+
+export const updateClass = async (
+    currentState: CurrentState,
+    data: ClassSchema
+) => {
+    try {
+        await prisma.class.update({
+            where: { id: data.id },
+            data: {
+                name: data.name,
+                supervisorId: data.supervisorId,
+                gradeId: data.gradeId,
+            },
+        });
+
+        // revalidatePath("/list/class")
+        return { success: true, error: false };
+    } catch (err) {
+        console.log(err)
+        return { success: false, error: true };
+    }
+};
+
+export const deleteClass = async (
+    currentState: CurrentState,
+    data: FormData
+) => {
+    const id = data.get("id") as string;
+    try {
+        await prisma.class.delete({
+            where: {
+                id: parseInt(id)
+            },
+        });
+
+        // revalidatePath("/list/class")
+        return { success: true, error: false };
+    } catch (err) {
+        console.log(err)
+        return { success: false, error: true };
+    }
+};
+
+// * ---------------------------------------------- TEACHER SCHEMA --------------------------------------------------------
+
+
+export const createTeacher = async (
+    currentState: CurrentState,
+    data: Teacherschema
+) => {
+    try {
+        await prisma.teacher.create({
+            data: {
+                name: data.name,
+                supervisorId: data.supervisorId,
+                gradeId: data.gradeId,
+            },
+        });
+
+        // revalidatePath("/list/teachers")
+        return { success: true, error: false };
+    } catch (err) {
+        console.log(err)
+        return { success: false, error: true };
+    }
+};
+
+export const updateTeacher = async (
+    currentState: CurrentState,
+    data: Teacherschema
+) => {
+    try {
+        await prisma.teacher.update({
+            where: { id: data.id },
+            data: {
+                name: data.name,
+                supervisorId: data.supervisorId,
+                gradeId: data.gradeId,
+            },
+        });
+
+        // revalidatePath("/list/teachers")
+        return { success: true, error: false };
+    } catch (err) {
+        console.log(err)
+        return { success: false, error: true };
+    }
+};
+
+export const deleteTeacher = async (
+    currentState: CurrentState,
+    data: FormData
+) => {
+    const id = data.get("id") as string;
+    try {
+        await prisma.teacher.delete({
+            where: {
+                id: parseInt(id)
+            },
+        });
+
+        // revalidatePath("/list/teachers")
+        return { success: true, error: false };
+    } catch (err) {
+        console.log(err)
+        return { success: false, error: true };
+    }
+};
+
