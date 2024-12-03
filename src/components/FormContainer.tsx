524ecb447ef6eda88ba5c17e9a74ede6d36c0e7e
Hari Kiran Adangi <harikiran.adangi@gmail.com>
2024-12-03 20:54:54 +0530

    Update FormContainer.tsx

--- src/components/FormContainer.tsx
+++ src/components/FormContainer.tsx
@@ -18,29 +18,53 @@ export type FormContainerProps = {
     type: "create" | "update" | "delete";
     data?: any;
     id?: number | string;
-  }
-
-const FormContainer = async ({table, type, data, id, }: FormContainerProps ) => {
-  
-    let relatedData = {}
-  
-if(type !== "delete"){
-    switch (table) {
-        case "subject":
-        
-        const subjectTeachers = await prisma.teacher.findMany({
-            select: {id:true, name: true, surname:true},
-        });
-        relatedData = { teachers: subjectTeachers}
-        break;
-
-    }
 }
+
+const FormContainer = async ({ table, type, data, id, }: FormContainerProps) => {
+
+    let relatedData = {}
+
+    if (type !== "delete") {
+        switch (table) {
+            case "subject":
+
+                const subjectTeachers = await prisma.teacher.findMany({
+                    select: { id: true, name: true, surname: true },
+                });
+
+
+                relatedData = { teachers: subjectTeachers }
+                break;
+
+            case "class":
+
+                const classGrades = await prisma.grade.findMany({
+                    select: { id: true, level: true },
+                });
+
+                const classTeachers = await prisma.teacher.findMany({
+                    select: { id: true, name: true, surname: true },
+                });
+
+                relatedData = { teachers: classTeachers, grades: classGrades }
+                break;
+            
+            default:
+              break;
+
+        }
+    }
     return (
-    <div className="container"><FormModal table={table} type={type} data={data} id={id} relatedData={relatedData}/>
-    
-    </div>
-  );
+        <div className="">
+            <FormModal 
+                table={table} 
+                type={type} 
+                data={data} 
+                id={id} 
+                relatedData={relatedData} 
+            />
+        </div>
+    );
 };
 
 export default FormContainer;
