96dbc8eca6ae6de171d82d6422f27614ad730595
Hari Kiran Adangi <harikiran.adangi@gmail.com>
2024-12-08 15:39:04 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      { hostname: "images.pexels.com" },
-      
+      {
+        protocol: "https", // Protocol of the external image
+        hostname: "images.pexels.com", // Hostname of the external source
+        pathname: "**", // Allow all paths
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com", // Add Cloudinary domain
+        pathname: "**", // Allow all paths from this domain
+      },
     ],
   },
 };
