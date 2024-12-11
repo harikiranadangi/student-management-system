b61b74c5e6f3533413b39badaa20a7ff529ef2ab
Hari Kiran Adangi <harikiran.adangi@gmail.com>
2024-12-11 22:01:18 +0530

    Update FormContainer.tsx

--- src/components/FormContainer.tsx
+++ src/components/FormContainer.tsx
@@ -1,5 +1,7 @@
 import prisma from "@/lib/prisma";
 import FormModal from "./FormModal";
+import { getRole } from "@/lib/utils";
+import { auth } from "@clerk/nextjs/server";
 
 export type FormContainerProps = {
     table:
@@ -62,12 +64,35 @@ const FormContainer = async ({ table, type, data, id, }: FormContainerProps) => 
                 const studentGrades = await prisma.grade.findMany({
                     select: { id: true, level: true },
                 });
-                
+
                 const studentClasses = await prisma.class.findMany({
-                    include: { _count: { select : { students: true } } },
+                    include: { _count: { select: { students: true } } },
                 });
 
                 relatedData = { classes: studentClasses, grades: studentGrades }
+                break;
+
+            case "exam":
+                const { userId, sessionClaims } = await auth()
+
+                const role = (
+                    sessionClaims?.metadata as {
+                        role?: "admin" | "teacher" | "student";
+                    }
+                )?.role;
+
+
+
+
+                const examLessons = await prisma.lesson.findMany({
+                    where: {
+                        ...(role === "teacher" ? { teacherId: userId! } : {}),
+                    },
+                    select: { id: true, name: true},
+                });
+
+                
+                relatedData = { lessons: examLessons }
                 break;
 
             default:
