098d8362da94f607f4ac57ba29dcf936a4ab9e5e
Hari Kiran Adangi <harikiran.adangi@gmail.com>
2024-12-12 19:04:04 +0530

    Update ClassForm.tsx

--- src/components/forms/ClassForm.tsx
+++ src/components/forms/ClassForm.tsx
@@ -4,7 +4,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import InputField from "../InputField";
 import { classSchema, ClassSchema, } from "@/lib/formValidationSchemas";
-import { createClass,  updateClass } from "@/lib/actions";
+import { createClass, updateClass } from "@/lib/actions";
 import React, { Dispatch, SetStateAction, useEffect } from "react";
 import { toast } from "react-toastify";
 import { useRouter } from "next/navigation";
@@ -20,10 +20,10 @@ const ClassForm = ({
     setOpen: Dispatch<SetStateAction<boolean>>;
     relatedData?: any
 }) => {
-    const { 
-        register, 
-        handleSubmit, 
-        formState: { errors }, 
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
     } = useForm<ClassSchema>({
         resolver: zodResolver(classSchema),
     });
@@ -32,7 +32,7 @@ const ClassForm = ({
 
     // Using useActionState with startTransition
     const [state, formAction] = React.useActionState(
-        type=== "create" ? createClass : updateClass, {
+        type === "create" ? createClass : updateClass, {
         success: false,
         error: false,
     });
@@ -69,8 +69,8 @@ const ClassForm = ({
                     register={register}
                     error={errors?.name}
                 />
-                
-               {data && (<InputField
+
+                {data && (<InputField
                     label="Id"
                     name="id"
                     defaultValue={data?.id}
@@ -79,50 +79,48 @@ const ClassForm = ({
                     hidden
                 />
                 )}
-                
+
                 <div className="flex flex-col w-full gap-2 md:w-1/4">
-                <label className="text-xs text-gray-500">Supervisor</label>
-                <select
-                    className="ring-[1.5px] ring-gray-300 p-2 rounded-md text-sm w-full"
-                    {...register("supervisorId")}
-                    defaultValue={data?.teachers || ""}
-                >
-                    {teachers.map(
-                        (teacher: {id: string; name: string; surname: string}) => (
-                        <option value={teacher.id} key={teacher.id}
-                        selected= {data && teacher.id === data.supervisorId}
-                        >
-                            {teacher.name + " " + teacher.surname}
-                        </option> 
-                    ))}
-                </select>
-                {errors.supervisorId?.message && (
-                    <p className="text-xs text-red-400">
-                        {errors.supervisorId.message.toString()}
-                    </p>
-                )} 
-              </div>
-                
+                    <label className="text-xs text-gray-500">Supervisor</label>
+                    <select
+                        className="ring-[1.5px] ring-gray-300 p-2 rounded-md text-sm w-full"
+                        {...register("supervisorId")}
+                        defaultValue={data?.supervisorId || ""}
+                    >
+                        {teachers.map((teacher: { id: string; name: string; surname: string }) => (
+                            <option value={teacher.id} key={teacher.id}>
+                                {`${teacher.name} ${teacher.surname}`}
+                            </option>
+                        ))}
+                    </select>
+
+                    {errors.supervisorId?.message && (
+                        <p className="text-xs text-red-400">
+                            {errors.supervisorId.message.toString()}
+                        </p>
+                    )}
+                </div>
+
                 <div className="flex flex-col w-full gap-2 md:w-1/4">
-                <label className="text-xs text-gray-500">Grade</label>
-                <select
-                    className="ring-[1.5px] ring-gray-300 p-2 rounded-md text-sm w-full"
-                    {...register("gradeId")}
-                    defaultValue={data?.gradeId || ""}
-                >
-                    {grades.map((grade: {id: number; level: number }) => (
-                        <option value={grade.id} key={grade.id} 
-                        selected= {data && grade.id === data.gradeId}
-                        >
-                        {grade.level}
-                        </option> 
-                    ))}
-                </select>
-                {errors.gradeId?.message && (
-                    <p className="text-xs text-red-400">{errors.gradeId.message.toString()}</p>
-                )} 
-              </div>
-                
+                    <label className="text-xs text-gray-500">Grade</label>
+                    <select
+                        className="ring-[1.5px] ring-gray-300 p-2 rounded-md text-sm w-full"
+                        {...register("gradeId")}
+                        defaultValue={data?.gradeId || ""}
+                    >
+                        {grades.map((grade: { id: number; level: number }) => (
+                            <option value={grade.id} key={grade.id}
+                                selected={data && grade.id === data.gradeId}
+                            >
+                                {grade.level}
+                            </option>
+                        ))}
+                    </select>
+                    {errors.gradeId?.message && (
+                        <p className="text-xs text-red-400">{errors.gradeId.message.toString()}</p>
+                    )}
+                </div>
+
             </div>
             {state.error && <span className="text-red-500">Something went wrong!</span>}
             <button className="p-2 text-white bg-blue-400 rounded-md">
