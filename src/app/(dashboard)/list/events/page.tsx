a377345a3c2e7b6a65f79ccbc53ab95cd72a3574
Hari Kiran Adangi <harikiran.adangi@gmail.com>
2024-12-13 20:45:59 +0530

    Update page.tsx

--- src/app/(dashboard)/list/events/page.tsx
+++ src/app/(dashboard)/list/events/page.tsx
@@ -98,7 +98,9 @@ const EventsList = async ({
       : []),
   ];
 
-  const { page, ...queryParams } = searchParams;
+  // Await the searchParams first
+  const params = await searchParams;
+  const { page, ...queryParams } = params;
   const p = page ? parseInt(page) : 1;
 
   // Initialize Prisma query object
