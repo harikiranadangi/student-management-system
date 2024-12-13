dd20c2d03ede5f895688670fdf8f07a3e28e3b10
Hari Kiran Adangi <harikiran.adangi@gmail.com>
2024-12-13 19:12:48 +0530

    Update page.tsx

--- src/app/(dashboard)/list/lessons/page.tsx
+++ src/app/(dashboard)/list/lessons/page.tsx
@@ -126,7 +126,7 @@ const LessonsListPage = async ({
             <button className="flex items-center justify-center w-8 h-8 rounded-full bg-LamaYellow">
               <Image src="/sort.png" alt="" width={14} height={14} />
             </button>
-            {role === "admin" && (
+            {role === "admin" || role === "teacher" && (
               <FormModal table="lesson" type="create" />
             )}
           </div>
