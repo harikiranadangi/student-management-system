163b990ef817f6fdee21e6eb4390808fa0293a89
Hari Kiran Adangi <harikiran.adangi@gmail.com>
2025-01-12 06:50:05 +0530

    Update FormModal.tsx

--- src/components/FormModal.tsx
+++ src/components/FormModal.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { deleteClass, deleteSubject, deleteTeacher, } from "@/lib/actions";
+import { deleteClass, deleteStudent, deleteSubject, deleteTeacher, } from "@/lib/actions";
 import dynamic from "next/dynamic";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
@@ -12,7 +12,7 @@ const deleteActionMap = {
   subject: deleteSubject,
   class: deleteClass,
   teacher: deleteTeacher,
-  student: deleteSubject,
+  student: deleteStudent,
   exam: deleteSubject,
   lesson: deleteSubject,
   assignment: deleteSubject,
