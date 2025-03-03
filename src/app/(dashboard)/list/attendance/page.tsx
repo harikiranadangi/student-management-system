bf7c00afbfa383c68e281eded576b258728f5913
Hari Kiran Adangi <harikiran.adangi@gmail.com>
2025-03-03 05:55:16 +0530

    Create page.tsx

---
+++ src/app/(dashboard)/list/attendance/page.tsx
@@ -0,0 +1,144 @@
+import FormContainer from "@/components/FormContainer";
+import Pagination from "@/components/Pagination";
+import Table from "@/components/Table";
+import TableSearch from "@/components/TableSearch";
+import prisma from "@/lib/prisma";
+import { ITEM_PER_PAGE } from "@/lib/settings";
+import { fetchUserInfo } from "@/lib/utils";
+import { Attendance, Class, Prisma, Student } from "@prisma/client";
+import Image from "next/image";
+
+type AttendanceList = Attendance & { student: Student; class: Class };
+
+const renderRow = (item: AttendanceList, role: string | null) => (
+  <tr key={item.id} className="text-sm border-b border-gray-200 even:bg-slate-50 hover:bg-LamaPurpleLight">
+    <td className="flex items-center gap-4 p-4">{item.student.name}</td>
+    <td>{item.class.name || "-"}</td>
+    <td className="hidden md:table-cell">
+      {new Intl.DateTimeFormat("en-US").format(item.date)}
+    </td>
+    <td>
+      <div className="flex items-center gap-2">
+        {role === "admin" || role === "teacher" && (
+          <>
+            <FormContainer table="announcement" type="update" data={item} />
+            <FormContainer table="announcement" type="delete" id={item.id} />
+          </>
+        )}
+      </div>
+    </td>
+  </tr>
+);
+
+const getColumns = (role: string | null) => [
+  {
+    header: "Date",
+    accessor: "date",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "Student Name",
+    accessor: "student.name",
+  },
+  {
+    header: "Class",
+    accessor: "class.name",
+  },
+  {
+    header: "Present",
+    accessor: "present",
+    className: "hidden md:table-cell",
+  },
+  ...(role === "admin"
+    ? [
+      {
+        header: "Actions",
+        accessor: "action",
+      },
+    ]
+    : []),
+];
+
+const AttendanceListPage = async ({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) => {
+
+  // Fetch user info and role
+  const { userId, role } = await fetchUserInfo();
+
+
+  const columns = getColumns(role);  // Get dynamic columns
+
+
+  // Await the searchParams first
+  const params = await searchParams;
+  const { page, ...queryParams } = params;
+  const p = page ? parseInt(page) : 1;
+
+  // Initialize Prisma query object
+  const query: Prisma.AttendanceWhereInput = {};
+
+  // Dynamically add filters based on query parameters
+  if (queryParams) {
+    for (const [key, value] of Object.entries(queryParams)) {
+      if (value !== undefined) {
+        switch (key) {
+          case "search":
+            query.OR = [
+              { Student: { name: { contains: value, mode: "insensitive" } } },
+              { studentId: { contains: value, mode: "insensitive" } },
+            ];
+
+            break;
+          default:
+            break;
+        }
+      }
+    }
+  }
+
+
+  // Fetch teachers and include related fields (subjects, classes)
+  const [data, count] = await prisma.$transaction([
+    prisma.attendance.findMany({
+      where: query,
+      include: {
+        Student: true,
+        class: true,
+      },
+
+      take: ITEM_PER_PAGE,
+      skip: ITEM_PER_PAGE * (p - 1),
+    }),
+    prisma.attendance.count({ where: query }),
+  ]);
+
+  return (
+    <div className="flex-1 p-4 m-4 mt-0 bg-white rounded-md">
+      {/* TOP: Description */}
+      <div className="flex items-center justify-between">
+        <h1 className="hidden text-lg font-semibold md:block">All Announcements</h1>
+        <div className="flex flex-col items-center w-full gap-4 md:flex-row md:w-auto">
+          <TableSearch />
+          <div className="flex items-center self-end gap-4">
+            <button className="flex items-center justify-center w-8 h-8 rounded-full bg-LamaYellow">
+              <Image src="/filter.png" alt="" width={14} height={14} />
+            </button>
+            <button className="flex items-center justify-center w-8 h-8 rounded-full bg-LamaYellow">
+              <Image src="/sort.png" alt="" width={14} height={14} />
+            </button>
+            {role === "admin" && <FormContainer table="announcement" type="create" />}
+          </div>
+        </div>
+      </div>
+      {/* LIST: Description */}
+      <Table columns={columns} renderRow={(item) => renderRow(item, role)} data={data} />
+      {/* PAGINATION: Description */}
+      <Pagination page={p} count={count} />
+    </div>
+  );
+};
+
+export default AttendanceListPage;
