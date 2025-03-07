26e35e5a360b1272dc25a55072e5e9c5e0d7b05a
Hari Kiran Adangi <harikiran.adangi@gmail.com>
2025-03-07 20:54:24 +0530

    Update formValidationSchemas.ts

--- src/lib/formValidationSchemas.ts
+++ src/lib/formValidationSchemas.ts
@@ -1,11 +1,12 @@
-import { date, z } from "zod";
+import { z } from "zod";
 
-// Define the schema with zod
+
 export const subjectSchema = z.object({
   id: z.coerce.number().optional(),
   name: z.string().min(1, { message: 'Subject Name is required!' }),
-  teachers: z.array(z.string()), // teacher id's
+  teachers: z.array(z.string().min(1, { message: 'Teacher ID cannot be empty' })).optional(),
 });
+
 
 // Infer the form data type from schema
 export type SubjectSchema = z.infer<typeof subjectSchema>;
