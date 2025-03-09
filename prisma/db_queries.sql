a19641ebf554a050dde189241a3648da1aea8f18
Hari Kiran Adangi <harikiran.adangi@gmail.com>
2025-03-09 15:47:37 +0530

    Update db_queries.sql

--- prisma/db_queries.sql
+++ prisma/db_queries.sql
@@ -6,6 +6,33 @@ SELECT 'SELECT * FROM ' || tablename || ';'
 FROM pg_tables  
 WHERE schemaname = 'public';
 
+CREATE TABLE IF NOT EXISTS clerk_users (
+    id VARCHAR PRIMARY KEY,
+    username VARCHAR UNIQUE NOT NULL,
+    password VARCHAR NOT NULL,
+    name VARCHAR NOT NULL,
+    surname VARCHAR NOT NULL
+);
+
+SELECT * FROM clerk_users;
+-- DROP TABLE IF EXISTS "Users" CASCADE;
+
+SELECT username FROM clerk_users WHERE username IN (
+    '9182610410', '8374970989', '9676334505', '8885890911', '9494645918', '9440424287'
+);
+
+
+-- "C:\Program Files\PostgreSQL\17\bin\psql.exe" -U postgres -d kotakdatabase
+
+DELETE FROM clerk_users;
+
+
+
+
+
+SELECT tablename FROM pg_tables WHERE schemaname = 'public';
+
+
 -- View all migration records from Prisma's migration table
 SELECT * FROM _prisma_migrations;
 
@@ -14,6 +41,21 @@ SELECT * FROM Admin;
 
 -- Retrieve all records from the class table
 SELECT * FROM class;
+
+SELECT * FROM "Teacher" WHERE id = 'T1';
+SELECT * FROM "Grade" WHERE id = 1;
+SELECT * FROM "class" ;
+
+INSERT INTO "class" ("name", "gradeId", "supervisorId") 
+VALUES ('test1', 5, 'T2') RETURNING *;
+
+
+SELECT setval('"class_id_seq"', (SELECT MAX(id) FROM "class"));
+
+
+SELECT * FROM "teacher" WHERE id = 'user_2u2pXhmNqzsHVzrbD2WbjYYfLsO';
+
+
 
 -- Retrieve all records from the Announcement table (incorrect syntax - should use double quotes)
 SELECT * FROM "Announcement";
