47504ccbd9008a6ec0ffa061b4bb371f8be732fa
Hari Kiran Adangi <harikiran.adangi@gmail.com>
2025-03-09 22:30:36 +0530

    Create migration.sql

---
+++ prisma/migrations/20250309165431_delete_surname/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `surname` on the `clerk_users` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "clerk_users" DROP COLUMN "surname";
