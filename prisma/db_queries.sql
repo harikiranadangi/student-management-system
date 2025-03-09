bfe998848c938a1e85523888a1c12a07dc5ad77a
Hari Kiran Adangi <harikiran.adangi@gmail.com>
2025-03-09 09:00:05 +0530

    Update db_queries.sql

--- prisma/db_queries.sql
+++ prisma/db_queries.sql
@@ -5,22 +5,38 @@ FROM pg_tables
 WHERE schemaname = 'public';
 
 SELECT * FROM _prisma_migrations;
+
 SELECT * FROM Admin;
+
 SELECT * FROM class;
+
 SELECT * FROM 'Announcement';
+
 SELECT * FROM "Student";
 
 SELECT * FROM "Grade";
+
 SELECT * FROM "Teacher";
+
 SELECT * FROM "Event";
+
 SELECT * FROM "Exam";
+
 SELECT * FROM "ExamSubject";
+
 SELECT * FROM "Subject";
 SELECT * FROM "Fee";
+
 SELECT * FROM "Homework";
+
 SELECT * FROM "Lesson";
+
 SELECT * FROM "Result";
+
 SELECT * FROM "Attendance";
+
 SELECT * FROM "FeesStructure";
+
 SELECT * FROM "StudentFees";
+
 SELECT * FROM "TeacherSubject";
