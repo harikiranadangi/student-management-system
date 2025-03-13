b49c69e2107ea6e761295910d3a39b90cd459363
Hari Kiran Adangi <harikiran.adangi@gmail.com>
2025-03-13 18:03:02 +0530

    Update page.tsx

--- src/app/(dashboard)/list/teachers/[id]/page.tsx
+++ src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -4,7 +4,6 @@ import FormContainer from "@/components/FormContainer";
 import Performance from "@/components/Performance";
 import prisma from "@/lib/prisma";
 import { auth } from "@clerk/nextjs/server";
-import { Teacher } from "@prisma/client";
 import Image from "next/image";
 import Link from "next/link";
 import { notFound } from "next/navigation";
