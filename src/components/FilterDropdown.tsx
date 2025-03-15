2e6ee66632937e200e75a58f0c5b6f8436c5b807
Hari Kiran Adangi <harikiran.adangi@gmail.com>
2025-03-15 23:04:16 +0530

    Update FilterDropdown.tsx

--- src/components/FilterDropdown.tsx
+++ src/components/FilterDropdown.tsx
@@ -21,24 +21,25 @@ const FilterDropdown = ({ classes }: { classes: ClassType[] }) => {
 
   return (
     <div className="relative w-full md:w-auto">
-  <select
-    className="w-full py-2 pl-10 pr-4 text-sm border border-gray-200 rounded-full appearance-none md:w-auto focus:ring-2 focus:ring-black-500 focus:outline-none"
-    onChange={handleChange}
-    defaultValue={searchParams.get('classId') || ''}
-  >
-    <option value="" disabled hidden>
-    Class
-  </option>
-    {classes.map((cls) => (
-      <option key={cls.id} value={cls.id}>
-        {cls.name}
-      </option>
-    ))}
-  </select>
-  <span className="absolute text-gray-400 transform -translate-y-1/2 left-2 top-1/2">
-  🔽 {/* You can replace this with an actual icon */}
-  </span>
-</div>
+      <select
+        className="w-full py-2 pl-4 pr-10 text-sm text-gray-500 border border-gray-300 rounded-full appearance-none md:w-auto focus:ring-2 focus:ring-blue-500 focus:outline-none"
+        onChange={handleChange}
+        defaultValue=""
+      >
+        <option value="" >
+          Class
+        </option>
+        {classes.map((cls) => (
+          <option key={cls.id} value={cls.id} className="text-gray">
+            {cls.name}
+          </option>
+        ))}
+      </select>
+
+      <span className="absolute text-gray-400 transform -translate-y-1/2 right-4 top-1/2">
+        🔽 {/* You can replace this with an actual icon */}
+      </span>
+    </div>
 
   );
 };
