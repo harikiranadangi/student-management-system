8c22b5b9b9442e101b4122993c3c49aff8dfa3e8
Hari Kiran Adangi <harikiran.adangi@gmail.com>
2025-03-17 07:04:58 +0530

    Update page.tsx

--- src/app/(dashboard)/list/fees/page.tsx
+++ src/app/(dashboard)/list/fees/page.tsx
@@ -16,8 +16,8 @@ type FeesList = Class & {
 const renderRow = (item: FeesList, role: string | null) => (
   <tr key={item.id} className="text-sm border-b border-gray-200 even:bg-slate-50 hover:bg-LamaPurpleLight">
     <td>{item.name}</td>
-    <td>{item.fees?.abacusFees}</td>
     <td>{item.fees?.termFees}</td>
+    <td>{item.fees?.abacusFees}</td>
     <td>{item.fees?.totalFees ?? "N/A"}</td>
     <td>
       <div className="flex items-center gap-2">
@@ -46,8 +46,8 @@ const FeesListPage = async ({
 
   const columns = [
     { header: "Class", accessor: "name" },
-    { header: "Abacus Fees", accessor: "fees.abacusFees" },
     { header: "Term Fees", accessor: "fees.termFees" },
+    { header: "Abacus Fees", accessor: "fees.abacusFees" },
     { header: "Total Fees", accessor: "fees.totalFees" },
     ...(role === "admin" ? [{ header: "Actions", accessor: "action" }] : []),
   ];
@@ -72,26 +72,33 @@ const FeesListPage = async ({
   // Rendering 
   return (
     <div className="flex-1 m-4 mt-0 bg-white rounded-md">
+      
       {/* TOP: Description */}
       <div className="flex items-center justify-between">
         <h1 className="hidden text-lg font-semibold md:block">Fees Structure ({count} Classes)</h1>
         <div className="flex flex-col items-center w-full gap-4 md:flex-row md:w-auto">
+
           <TableSearch />
           <div className="flex items-center self-end gap-4">
+
             <button className="flex items-center justify-center w-8 h-8 rounded-full bg-LamaYellow">
               <Image src="/filter.png" alt="" width={14} height={14} />
             </button>
+
             <button className="flex items-center justify-center w-8 h-8 rounded-full bg-LamaYellow">
               <Image src="/sort.png" alt="" width={14} height={14} />
             </button>
+
             {(role === "admin") && (
               <FormContainer table="fees" type="create" />
             )}
           </div>
         </div>
       </div>
+
       {/* LIST: Fees Table */}
       <Table columns={columns} renderRow={(item) => renderRow(item, role)} data={data} />
+        
       {/* PAGINATION */}
       <Pagination page={p} count={count} />
     </div>
